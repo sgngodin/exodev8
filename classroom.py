6ef40bf753247a9678893361d8455107bdb941ae
Stephane <pub.sg@free.fr>
2023-02-14 14:46:19 +0100

    jessica modif

--- classroom.py
+++ classroom.py
@@ -2,7 +2,7 @@ import stephane
 import theau
 import yaniss
 import benjamin
-import Jessica
+import jessica
 import stephanie
 import julick
 import patrick
@@ -13,7 +13,7 @@ print(stephane.Prenom(), stephane.Nom())
 print(theau.Prenom(), theau.Nom())
 print(yaniss.Prenom(), yaniss.Nom())
 print(benjamin.Prenom(), benjamin.Nom())
-print(Jessica.Prenom(), Jessica.Nom())
+print(jessica.Prenom(), jessica.Nom())
 print(morgan.Prenom(), morgan.Nom())
 print(julick.Prenom(), julick.Nom())
 print(patrick.Prenom(), patrick.Nom())
